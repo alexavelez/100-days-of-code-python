513acb417cebf888d278b8e8d0542b46df2970b9
alexavelez <143228567+alexavelez@users.noreply.github.com>
2024-09-19 22:18:09 -0400

    Add files via upload

---
+++ Day_7/hangman_art.py
@@ -0,0 +1,86 @@
+hangman = ['''
+   +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''']
+
+encouraging_messages = [
+    "Oops, not quite! You've got this!",
+    "Close, but no prize. have another go!",
+    "Not the right letter, but you'll do better next time!",
+    "Nice try! You'll crack this soon!",
+    "Whoops! No worries, try again!",
+    "Almost there! You can do it!",
+    "Not this time, but don't give up!",
+    "So close!",
+    "Keep calm, there is always tomorrow!",
+    "Good effort! Now, give it another shot.",
+    "Wrong letter, but your luck could turn!",
+    "Don’t worry, everyone makes mistakes!",
+    "Breathe, regroup, and try again!",
+    "You’re learning the ropes. Try once more!",
+    "Whoops! Think of it as a practice round!"
+]
+
+correct_guess_messages = [
+    "Good guess!",
+    "That's correct!",
+    "Good job!",
+    "You're on fire!",
+    "Nice one!",
+    "Well done!",
+    "You nailed it!",
+    "Keep it up!",
+    "Great choice!",
+    "You're getting closer!",
+    "Fantastic guess!",
+    "Spot on!",
+    "You're crushing it!",
+    "Awesome! Keep going!",
+    "You're a hangman pro!"
+]
