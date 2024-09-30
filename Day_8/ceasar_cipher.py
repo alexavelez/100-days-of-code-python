ec54aa5457c0cb30d41850ce3f2fd8cbc5854127
alexavelez <143228567+alexavelez@users.noreply.github.com>
2024-09-30 18:41:57 -0400

    Update and rename ceasar_cipher to ceasar_cipher.py

--- Day_8/ceasar_cipher.py
+++ Day_8/ceasar_cipher.py
@@ -4,37 +4,40 @@ print(caesar_cipher_art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+# Defining the cipher/decypher function
 def caesar(direction, text, shift):
     result = ""
     encrypted_decrypted = ""
-    # Result has to be declared inside the function, otherwise error
     for letter in text:
         if letter in alphabet:
+            #Encrypting
             if direction == "encode":
                 index = alphabet.index(letter) + shift
                 encrypted_decrypted = "encrypted"
+            #Decrypting
             else:
                 index = alphabet.index(letter) - shift
                 encrypted_decrypted == "decrypted"
-                # An alternative way is to leave this "index = alphabet.index(letter) + shift" out of the if statement, if direction == "decode: multiply by -1
-                # index *= -1
             index = index % len(alphabet)
             # We use modulo to deal with lengths> 25. If <25, it will return the same number, if > it will return the exact position after 25, no matter the times it fits into it.
-            # Same but shorter: index %= len(alphabet)
             result += alphabet[index]
+            #If the input string contains symbols or numbers, the else statement will add them to the result.
         else:
             result += letter
     print(f"This is your {encrypted_decrypted} message: {result}.\n")
 
-
+#Adding a continue_cipher variable to use in the loop as True or False to exit the while loop
 continue_cipher = True
 
+#Using a while loop to continue encoding/decoding
 while continue_cipher:
     direction = input('Type "encode" to encrypt, type "decode" to decrypt:\n').lower()
+    #Checking correct encode/decode input
     if direction != "encode" and direction != "decode":
         print("Wrong input.")
         continue
     text = input('Type your message:\n').lower()
+    #Checking the shift input is a number
     shift =  0
     try:
         shift = int(input("Type the shift number:\n"))
